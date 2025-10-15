bc556a118e185e26cf2df75abe7dd64e055fbb00
georegmontero1211@gmail.com <georgemontero1211@gmail.com>
2025-10-14 22:58:57 -0400

    CRUD de los proyectos creado exitosamente

--- src/controllers/Project.controller.ts
+++ src/controllers/Project.controller.ts
@@ -11,11 +11,55 @@ export class ProjectController {
       console.log(error);
     }
   };
-
   static getAllProjects = async (req: Request, res: Response) => {
     try {
       const projects = await Project.find({});
       res.json(projects);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  static getProjectById = async (req: Request, res: Response) => {
+    const { id } = req.params;
+    try {
+      const project = await Project.findById(id);
+
+      if (!project) {
+        const error = new Error("Project not found");
+        return res.status(404).json({ error: error.message });
+      }
+      res.json(project);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  static updateProject = async (req: Request, res: Response) => {
+    const { id } = req.params;
+    try {
+      const project = await Project.findByIdAndUpdate(id, req.body);
+
+      if (!project) {
+        const error = new Error("Project not found");
+        return res.status(404).json({ error: error.message });
+      }
+
+      await project.save();
+      res.json(`Projecto: ${project.projectName} actualizado con exito`);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  static deleteProject = async (req: Request, res: Response) => {
+    const { id } = req.params;
+    try {
+      const project = await Project.findById(id);
+
+      if (!project) {
+        const error = new Error("Project not found");
+        return res.status(404).json({ error: error.message });
+      }
+      await project.deleteOne();
+      res.json(`Projecto: ${project.projectName} eliminado`);
     } catch (error) {
       console.log(error);
     }
--- src/routes/projects.routes.ts
+++ src/routes/projects.routes.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { ProjectController } from "../controllers/Project.controller";
-import { body } from "express-validator";
+import { body, param } from "express-validator";
 import { handleInputErrors } from "../middleware/validation";
 
 const projectRoutes = Router();
@@ -14,5 +14,26 @@ projectRoutes.post(
   ProjectController.createProject
 );
 projectRoutes.get("/", ProjectController.getAllProjects);
+projectRoutes.get(
+  "/:id",
+  param("id").isMongoId().withMessage("Id invalid"),
+  handleInputErrors,
+  ProjectController.getProjectById
+);
+projectRoutes.put(
+  "/:id",
+  param("id").isMongoId().withMessage("Id invalid"),
+  body("projectName").notEmpty().withMessage("Project name is required"),
+  body("descripcion").notEmpty().withMessage("Descripcion is required"),
+  body("clientName").notEmpty().withMessage("clientName is required"),
+  handleInputErrors,
+  ProjectController.updateProject
+);
+projectRoutes.delete(
+  "/:id",
+  param("id").isMongoId().withMessage("Id invalid"),
+  handleInputErrors,
+  ProjectController.deleteProject
+);
 
 export default projectRoutes;
